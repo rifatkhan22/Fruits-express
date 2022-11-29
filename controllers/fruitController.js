591d417de660a2c87f0e4eb118f64661f8546a17
khanr97 <tafirnahk@gmail.com>
2022-11-29 11:39:20 -0500

    styling and seed route

--- controllers/fruitController.js
+++ controllers/fruitController.js
@@ -7,6 +7,31 @@ const Fruit = require('../models/fruits')
 //I.N.D.U.C.E.S
 //Index, New, Delete, Update, Create, Edit, Show
 
+//Seed Route========================
+router.get("/seed",(req,res) => {
+    Fruit.create([
+        {
+            name: 'grapefruit',
+            color: 'pink',
+            readyToEat: true
+        },
+        {
+            name: 'grape',
+            color: 'purple',
+            readyToEat: false
+        },
+        {
+            name: 'avocado',
+            color: 'green',
+            readyToEat: true
+        }
+    ], (err,data)=> {
+        res.redirect("/fruits")
+    })
+})
+
+
+
  //=========FRUITS========================
  //INDEX=========================================
  router.get("/", (req, res) => {
--- server.js
+++ server.js
@@ -50,6 +50,9 @@ app.use((req, res, next) => {
   
 app.use(express.urlencoded({extended: false}))
 app.use(methodOverride("_method"))
+//tells express to try to match requests with files in the directory called 'public'
+app.use(express.static('public')); 
+
 
 //===========ROUTES===========
 //every single route in fruitcontroller will use thus
